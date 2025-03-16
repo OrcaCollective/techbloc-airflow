a43988c1b6396f7cf12759d8ee27cbc75765426f
Madison Swain-Bowden <bowdenm@spu.edu>
2025-03-15 17:08:36 -0700

    New DAG: offsite backups for docker volumes

---
+++ techbloc_airflow/dags/maintenance/backups/offsite_configs.py
@@ -0,0 +1,40 @@
+from dataclasses import dataclass, field
+from textwrap import dedent
+
+
+@dataclass
+class OffsiteConfig:
+    name: str
+    folder: str
+    volume: str
+    backup_folder: str = "~/backups"
+
+    filename: str = field(init=False)
+    final_path: str = field(init=False)
+    command: str = field(init=False)
+
+    def __post_init__(self):
+        self.filename = f"{self.name}-backup.tar.bz2"
+        self.final_path = f"{self.backup_folder}/{self.filename}"
+        self.command = dedent(f"""
+        cd {self.folder} && \
+        docker run --rm \
+            -v {self.volume}:/volume \
+            -v {self.backup_folder}:/backup \
+            loomchild/volume-backup \
+            backup {self.filename}
+        """)
+
+
+OFFSITE_CONFIGS = [
+    OffsiteConfig(
+        name="matrix",
+        folder="synapse",
+        volume="synapse_synapse_pg_data",
+    ),
+    OffsiteConfig(
+        name="openoversight",
+        folder="OpenOversight",
+        volume="openoversight_postgres",
+    ),
+]
